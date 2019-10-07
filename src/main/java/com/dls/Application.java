86ffc58a8696de60f47a2dc707f3ed96a70bb383
Zhang Jinhang <1747002392@qq.com>
2019-10-07 12:32:14 +0800

    first commit,application init

---
+++ src/main/java/com/dls/Application.java
@@ -0,0 +1,23 @@
+package com.dls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}

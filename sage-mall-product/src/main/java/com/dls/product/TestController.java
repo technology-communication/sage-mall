4b5af06f180dc8b2a5d1860c39f900aa7e729b78
royzhang <roy.zhang@yunzhihui.com>
2020-12-21 09:36:20 +0800

    添加nacos

--- sage-mall-product/src/main/java/com/dls/product/TestController.java
+++ sage-mall-product/src/main/java/com/dls/product/TestController.java
@@ -2,15 +2,12 @@ package com.dls.product;
 
 import com.dls.product.entity.Product;
 import com.dls.product.mapper.ProductMapper;
-import com.sun.xml.internal.ws.client.sei.ResponseBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.xml.ws.Response;
 
 /**
  * @author 张金行
--- sage-mall-web/src/main/java/com/dls/web/WebApplication.java
+++ sage-mall-web/src/main/java/com/dls/web/WebApplication.java
@@ -2,6 +2,7 @@ package com.dls.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 张金行
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2019/10/14 20:44
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class);
